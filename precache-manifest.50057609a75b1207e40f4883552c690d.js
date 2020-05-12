cf1f5ed7819e253b9a6b57b1f02d055b92fe3f57
singha-cz <rene.kostka@singha.cz>
2020-05-12 21:19:25 +0200

    Updates

---
+++ precache-manifest.50057609a75b1207e40f4883552c690d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5153e91ad897efd1c42eb405f2eb6c2f",
+    "url": "/a-todo/index.html"
+  },
+  {
+    "revision": "577b9deb4724faeb5d6d",
+    "url": "/a-todo/static/css/main.9db17dd8.chunk.css"
+  },
+  {
+    "revision": "94b00f541da5d80b3456",
+    "url": "/a-todo/static/js/2.e4be9b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a-todo/static/js/2.e4be9b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577b9deb4724faeb5d6d",
+    "url": "/a-todo/static/js/main.fb15dfa2.chunk.js"
+  },
+  {
+    "revision": "83045ec64ce6bba40c9d",
+    "url": "/a-todo/static/js/runtime-main.daa74c4a.js"
+  }
+]);
