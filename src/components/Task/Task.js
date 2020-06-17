780dbcce51295ad877aceb48ed77a64d0c89afb9
rkostka <rene.kostka@d3soft.cz>
2020-06-17 14:28:27 +0200

    task name wrapping

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -31,13 +31,14 @@ const Task = (props) => {
 
    return (
       <li className={css.taskItem}>
-         <span>
+         <div>
             <Button link onClick={() => complete(id, toDoListId)} title={completed ? "Mark incompleted" : "Mark completed"}>
                <FontAwesomeIcon color={completed ? css.primary : ""} size="lg" icon={["far", completed ? "check-circle" : "circle"]} />
             </Button> <TaskName editable={editable} handlers={{remove: remove, doSave: doSave, setEditable: setEditable}} {...props} />
-         </span> <span className={css.remove}>
-            <Button title="Delete" link onClick={() => remove(id, toDoListId)} icon={"times"} />
-         </span>
+            <span className={css.remove}>
+               <Button title="Delete" link onClick={() => remove(id, toDoListId)} icon={"times"} />
+            </span>
+         </div> 
       </li>
    );
 }
